158c25bfe8e49acd07cb81ddc89b196b60b97c32
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2021-09-08 17:59:02 +0500

    add cart update and get cart functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,7 +2,27 @@ var Cart = require("../models/cart");
 
 exports.cart_list = async (req, res) => {
   try {
-    const data = await Cart.find()
+    const data = await Cart.find().populate("product").exec();
+    res.json({ data, success: true });
+  } catch (error) {
+    res.json({ message: error.message, success: false });
+  }
+};
+
+exports.user_cart = async (req, res) => {
+  try {
+    const data = await Cart.find({ user: req.query.user })
+      .populate("product")
+      .exec();
+    console.log("data", req.query);
+    res.json({ data, success: true });
+  } catch (error) {
+    res.json({ message: error.message, success: false });
+  }
+};
+exports.user_current_cart = async (req, res) => {
+  try {
+    const data = await Cart.find({ user: req.query.user, status: "added" })
       .populate("product")
       .exec();
     res.json({ data, success: true });
@@ -10,28 +30,6 @@ exports.cart_list = async (req, res) => {
     res.json({ message: error.message, success: false });
   }
 };
-
-exports.user_cart= async (req,res)=>{  
-  try {
-    const data = await Cart.find({ user: req.query.user })
-      .populate("product")
-      .exec();
-      console.log("data",req.query)
-    res.json({ data, success: true });
-  } catch (error) {
-    res.json({ message: error.message, success: false });
-  }
-}
-exports.user_current_cart= async (req,res)=>{  
-  try {
-    const data = await Cart.find({ user: req.query.user, status:'added' })
-      .populate("product")
-      .exec();
-    res.json({ data, success: true });
-  } catch (error) {
-    res.json({ message: error.message, success: false });
-  }
-}
 
 exports.cart_detail = async (req, res) => {
   try {
@@ -45,7 +43,19 @@ exports.cart_detail = async (req, res) => {
 exports.add_cart = async (req, res) => {
   try {
     // console.log(req.user);
-    req.body.user;
+    // req.body.user;
+    if (req?.body?.user) {
+      let data = await Cart.findOne({
+        user: req?.body?.user,
+        product: req?.body?.product,
+        status: "added",
+      }).exec();
+      if (data) {
+        res.json({ data, success: true });
+        // res.json({})
+        return;
+      }
+    }
     let data = new Cart(req.body);
     data = await data.save();
     res.json({ data, success: true });
@@ -63,11 +73,9 @@ exports.remove_cart_by_cart_id = async (req, res) => {
 };
 exports.update_cart = async (req, res) => {
   try {
-    const data = await Cart.findByIdAndUpdate(
-      req.body.id,
-      req.body.updatedCart
-    ).exec();
-    res.json({ data, success: true });
+    const oldData = await Cart.findByIdAndUpdate(req.body._id, req.body);
+    const data = await Cart.findById(req.body._id);
+    res.json({ data, oldData, success: true });
   } catch (error) {
     res.json({ message: error.message, success: false });
   }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,14 @@
 var User = require("../models/user");
-bcrypt = require('bcrypt'),
-
-exports.user_list = async (req, res) => {
-  try {
-    console.log(req.user);
-    const data = await User.find().exec();
-    res.json({ data, success: true });
-  } catch (error) {
-    res.json({ message: error.message, success: false });
-  }
-};
+(bcrypt = require("bcrypt")),
+  (exports.user_list = async (req, res) => {
+    try {
+      console.log(req.user);
+      const data = await User.find().exec();
+      res.json({ data, success: true });
+    } catch (error) {
+      res.json({ message: error.message, success: false });
+    }
+  });
 exports.user_detail = async (req, res) => {
   try {
     const data = await User.findOne(req.body.id).exec();
@@ -33,32 +32,30 @@ exports.user_signup = async (req, res) => {
     data = await data.save();
     res.json({ data, success: true });
   } catch (error) {
-    try{
-      if(error.name=="ValidationError"){
-        if(error?.errors?.email?.message){
-          res.json({ message: error.errors.email.message, success: false });
+    try {
+      if (error.name == "ValidationError") {
+        if (error?.errors?.email?.message) {
+          res.json({ message: error?.errors?.email?.message, success: false });
+        } else if (error?.errors?.id?.message) {
+          res.json({ message: error?.errors?.id?.message, success: false });
         }
-        else if(error?.errors?.id?.message){
-          res.json({ message: error.errors.id.message, success: false });
-        }
-      }
-      else throw new Error('Value does not exist')
-    }
-    catch(e){
+      } else throw new Error("Value does not exist");
+    } catch (e) {
       res.json({ message: error.message, success: false });
-
     }
   }
 };
- 
-exports.user_login = async (req, res,next) => {
+
+exports.user_login = async (req, res, next) => {
   try {
-    const data = await User.findOne({email: req.body.email}).exec()
-    
-    const passwordMatch=await bcrypt.compare(req?.body?.passwordHash, data.passwordHash);
-    if(passwordMatch)
-      res.json({ data, success: true,passwordMatch });
-    else throw new Error("Password Not Matched")
+    const data = await User.findOne({ email: req.body.email }).exec();
+
+    const passwordMatch = await bcrypt.compare(
+      req?.body?.passwordHash,
+      data.passwordHash
+    );
+    if (passwordMatch) res.json({ data, success: true, passwordMatch });
+    else throw new Error("Password Not Matched");
   } catch (error) {
     res.json({
       message: error.message,
