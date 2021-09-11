5fe7047e772fa63ea31ba839019c9882249b6ece
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2021-09-11 11:09:32 +0500

    format pages

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
-var uniqueValidator = require('mongoose-unique-validator')
+var uniqueValidator = require("mongoose-unique-validator");
 var Schema = mongoose.Schema;
-bcrypt = require('bcrypt'),
-SALT_WORK_FACTOR = 10;
+(bcrypt = require("bcrypt")), (SALT_WORK_FACTOR = 10);
 
 var UserSchema = new Schema({
   id: { type: String },
@@ -26,43 +25,44 @@ var UserSchema = new Schema({
   },
   imageUrl: { type: String },
   passwordHash: { type: String },
-  
+
   // loginInfo: { type: Object, required: true },
 });
-UserSchema.path('id').required(function(){
-  return this.loginType!=='email'
-})
-UserSchema.path('email').required(function() {
-  return this.loginType === 'email'
-}) 
-UserSchema.pre('save', function(next) {
+UserSchema.path("id").required(function () {
+  return this.loginType !== "email";
+});
+UserSchema.path("email").required(function () {
+  return this.loginType === "email";
+});
+UserSchema.pre("save", function (next) {
   var user = this;
 
   // only hash the password if it has been modified (or is new)
-  if (!user.isModified('passwordHash')) return next();
+  if (!user.isModified("passwordHash")) return next();
 
   // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) {
+      console.log("erroooor");
+      return next(err);
+    }
+    // hash the password using our new salt
+    bcrypt.hash(user.passwordHash, salt, function (err, hash) {
       if (err) {
-        console.log("erroooor")
+        console.log("erroooor2");
         return next(err);
       }
-      // hash the password using our new salt
-      bcrypt.hash(user.passwordHash, salt, function(err, hash) {
-          if (err) {
-            console.log("erroooor2")            
-            return next(err)
-          }
-          // override the cleartext password with the hashed one
-          user.passwordHash = hash;
-          next();
-      });
+      // override the cleartext password with the hashed one
+      user.passwordHash = hash;
+      next();
+    });
   });
 });
-   
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
   // console.log(candidatePassword)
-  
 };
-UserSchema.plugin(uniqueValidator, {message:"This email already exists, try to login instead"});
+UserSchema.plugin(uniqueValidator, {
+  message: "This email already exists, try to login instead",
+});
 module.exports = mongoose.model("User", UserSchema);
