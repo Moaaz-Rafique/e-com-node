f07cb5e1b966196a7844dd6593262a34ab621a71
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2021-09-14 17:00:29 +0500

    add dotenv and mongoDB path as an env variable

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ var mongoose = require("mongoose");
 //Set up default mongoose connection
 var mongoDB = process.env.MONGO_DB;
 
-// console.log(mongoDB);
-// console.log("from app js process", process.env.MONGO_DB);
+console.log(mongoDB);
+console.log("from config js process", process.env.MONGO_DB);
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
